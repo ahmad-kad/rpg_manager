ab15bc684efd3eb9384770159bd98ae3ad7ceb20
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-03-18 17:46:22 -0700

    Initial Commit

---
+++ rpg_manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rpg_manager
+//
+//  Created by ahmad kaddoura on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, worlasdasdasd!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rpg_manager/rpg_managerApp.swift
@@ -0,0 +1,18 @@
+//
+//  rpg_managerApp.swift
+//  rpg_manager
+//
+//  Created by ahmad kaddoura on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct rpg_managerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
